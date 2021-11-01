2e7f2c7ea74c8beb12c450a9a850c42ad1eb976b
GamerMov123 <87977697+GamerMov123@users.noreply.github.com>
2021-11-01 13:43:25 +0530

    Update 111.cpp

--- 111.cpp
+++ 111.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
 using namespace std;
 int main(){
-	string name;
-	cout << "Hello! What is your name?";
-	cin >> name;
-	cout << "Hello " << name << "!";
+	int num1;
+	cout << "Enter a number for the prime test: \n";
+	cin >> num1;
+	if(num1 % 2 | 3 | 5 | 7 == 0){
+		cout << "It is a prime number.";
+	}
+	else{
+		cout << "It is not a prime number";
+	}
 	return 0;
 }
